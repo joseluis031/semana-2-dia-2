003fc65fb7e2a82cb4257b01910a5f8a4af8698f
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-10-24 10:49:23 +0200

    saxon creado

--- ejercicio.py
+++ ejercicio.py
@@ -19,6 +19,7 @@ class Viking(Soldier):
         self.name = name
 
     def receiveDamage_method(self,damage):
+        self.damage = damage
         self.health = self.health-self.damage
         
         if self.health > 0:
@@ -28,3 +29,17 @@ class Viking(Soldier):
             
     def battleCry_method():
         return "Odin Owns You All!"
+    
+
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(strength,health)
+        
+    def receiveDamage_method(self,damage):
+        self.damage = damage
+        self.health = self.health-self.damage
+        
+        if self.health > 0:
+            print("A Saxon has received",self.damage,"points of damage")
+        else:
+            print("A Saxon has died in act of combat")
