d3440c728fa39e3679aa39db50f341c43e269671
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-10-30 22:51:37 +0100

    probando test

--- ejercicio.py
+++ ejercicio.py
@@ -2,15 +2,15 @@
 from random import choice
 
 
-class Soldier:
+class Soldier():
     def __init__(self, health, strength):
         self.health = health
         self.strength = strength
     
-    def attact_method(self):
+    def attact(self):
         return self.strength
     
-    def receiveDamage_method(self,damage):
+    def receiveDamage(self,damage):
         self.damage = damage
         
         self.health = self.health-self.damage
@@ -18,19 +18,20 @@ class Soldier:
 
 class Viking(Soldier):
     def __init__(self,name, health, strength):
-        super().__init__(strength,health)
         self.name = name
+        super().__init__(strength,health)
 
-    def receiveDamage_method(self,damage):
+
+    def receiveDamage(self,damage):
         self.damage = damage
         self.health = self.health-self.damage
         
         if self.health > 0:
-            print(self.name," has received",self.damage,"points of damage")
+            return self.name," has received",self.damage,"points of damage"
         else:
-            print(self.name,"has died in act of combat")
+            return self.name,"has died in act of combat"
             
-    def battleCry_method():
+    def battleCry(self):
         return "Odin Owns You All!"
     
 
@@ -38,14 +39,14 @@ class Saxon(Soldier):
     def __init__(self, health, strength):
         super().__init__(strength,health)
         
-    def receiveDamage_method(self,damage):
+    def receiveDamage(self,damage):
         self.damage = damage
         self.health = self.health-self.damage
         
         if self.health > 0:
-            print("A Saxon has received",self.damage,"points of damage")
+            return "A Saxon has received",self.damage,"points of damage"
         else:
-            print("A Saxon has died in act of combat")
+            return "A Saxon has died in act of combat"
             
             
 class War:
@@ -64,7 +65,7 @@ class War:
     def vikingAttack(self):
         saxon = choice(self.saxonArmy)
         viking = choice(self.vikingArmy)
-        ataque = saxon.receiveDamage_method(viking.strength)
+        ataque = saxon.receiveDamage(viking.strength)
         if saxon.health <=0:
             self.saxonArmy.remove(saxon)
         else:
@@ -74,7 +75,7 @@ class War:
     def saxonAttack(self):
         saxon = choice(self.saxonArmy)
         viking = choice(self.vikingArmy)
-        ataque = viking.receiveDamage_method(saxon.strength)
+        ataque = viking.receiveDamage(saxon.strength)
         if viking.health <=0:
             self.vikingArmy.remove(viking)
         else:
